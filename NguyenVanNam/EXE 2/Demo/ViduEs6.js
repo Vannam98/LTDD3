3e91924e7da4e63c4276d6ca625c50a22b0c61a5
Nguyễn Văn Nam <43204587+Vannam98@users.noreply.github.com>
2019-10-31 07:31:27 +0700

    update

--- NguyenVanNam/EXE 2/Demo/ViduEs6.js
+++ NguyenVanNam/EXE 2/Demo/ViduEs6.js
@@ -1,15 +1,50 @@
-
-var mang = [4, 7, 5, 6, 2, 9, 13, 27, 10];
-function tinhTongSoLe(mang)
+// var mang = [4, 7, 5, 6, 2, 9, 13, 27, 10];
+// function tinhTongSoLe(mang)
+// {
+//     var tong = 0;
+//     for(var i = 0; i < mang.length; i++)
+//     {
+//         if(mang[i] % 2 != 0)
+//         {
+//             tong += mang[i];
+//         }
+//     }
+//     return tong;
+// }
+// console.log("Tổng các số lẻ = " + tinhTongSoLe(mang));
+var chuoi1 = "Hello";
+var chuoi2 = "TDC-Cao đẳng công nghệ";
+function soSanh()
 {
-    var tong = 0;
-    for(var i = 0; i < mang.length; i++)
+    if(chuoi1 > chuoi2)
     {
-        if(mang[i] % 2 != 0)
-        {
-            tong += mang[i];
-        }
+        console.log("chuỗi 1 dài hơn chuỗi 2")
     }
-    return tong;
+    else
+    {
+        console.log("chuỗi 1 ngắn hơn chuỗi 2")
+    }
 }
-console.log("Tổng các số lẻ = " + tinhTongSoLe(mang));
+soSanh();
+
+function noiChuoi()
+{
+    var chuoi = "Welcome";
+    var chuoi1 = " Mình là Nam đến từ TDC";
+    chuoi = chuoi.concat(chuoi1, "!!!");
+    console.log("Kết quả sau khi nối chuỗi: " +chuoi);
+}
+noiChuoi();
+
+function timKiemThayThe()
+{
+    var chuoi = chuoi1 + chuoi2;
+    console.log(chuoi.replace("TDC-Cao đẳng công nghệ"," Trường cao đẳng công nghệ thủ đức"));
+}
+timKiemThayThe();
+
+function viTri()
+{
+    console.log("vị trí xuất hiện chuỗi: " + chuoi2.indexOf("Cao"));
+}
+viTri();
