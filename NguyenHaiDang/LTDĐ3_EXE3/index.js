eaf685606500fb69ccc79fa6ea01ddb0b771482a
DangNguyen296 <43943018+DangNguyen296@users.noreply.github.com>
2019-10-31 07:46:36 +0700

    EXE

---
+++ NguyenHaiDang/LTDĐ3_EXE3/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import Hello from './Hello';
+AppRegistry.registerComponent(appName, () => Hello);
